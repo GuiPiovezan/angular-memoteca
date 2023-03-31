a5c611cb2a659a9270af9033d62e98f38a1ecec7
GuiPiovezan <guilherme.piovezan00@gmail.com>
2023-03-31 18:17:36 -0300

    Funcionalidades adicionadas como excluir e editar

--- src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -9,24 +11,28 @@ import { Pensamento } from '../pensamento';
 export class CriarPensamentoComponent implements OnInit {
 
   pensamento: Pensamento = {
-    id: 1,
-    conteudo: 'Aprendendo angular',
-    autoria: 'Dev',
-    modelo: ''
+    conteudo: '',
+    autoria: '',
+    modelo: 'modelo1'
   }
 
-  constructor() { }
+  constructor(
+    private service: PensamentoService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
 
   public criarPensamento(): void {
-    alert("Novo pensamento criado!")
+    this.service.salvar(this.pensamento).subscribe(() => {
+      this.router.navigate(['/listarPensamento'])
+    });
   }
 
   public cancelar(): void {
-    alert("Criação do pensamento cancelado!")
+    this.router.navigate(['/listarPensamento']);
   }
 
 }
--- src/app/components/pensamentos/pensamento.service.ts
+++ src/app/components/pensamentos/pensamento.service.ts
@@ -1,10 +1,37 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http'
+import { Pensamento } from './pensamento';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PensamentoService {
 
+  private readonly basePath = 'http://localhost:3000/pensamentos';
+
   constructor(private http: HttpClient) { }
+
+  listar(): Observable<Pensamento[]> {
+    return this.http.get<Pensamento[]>(this.basePath);
+  }
+
+  salvar(pensamento: Pensamento): Observable<Pensamento> {
+    return this.http.post<Pensamento>(this.basePath, pensamento);
+  }
+
+  editar(pensamento: Pensamento):Observable<Pensamento> {
+    const url = `${this.basePath}/${pensamento.id}`;
+    return this.http.put<Pensamento>(url, pensamento);
+  }
+
+  excluir(id: number):Observable<Pensamento> {
+    const url = `${this.basePath}/${id}`;
+    return this.http.delete<Pensamento>(url);
+  }
+
+  buscarPorId(id: number): Observable<Pensamento>{
+    const url = `${this.basePath}/${id}`;
+    return this.http.get<Pensamento>(url);
+  }
 }
